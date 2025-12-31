a10688b81686367cd19b2326973ff6c490a6138e
letmex <41751039+letmex@users.noreply.github.com>
2025-12-31 10:12:28 +0800

    Adjust optimizer logging and checkpointing

--- source/fit.py
+++ source/fit.py
@@ -28,17 +28,20 @@ class EarlyStopping:
 
 
 
-def fit(field_comp, training_set_collocation, T_conn, area_T, hist_alpha, matprop, pffmodel, 
+def fit(field_comp, training_set_collocation, T_conn, area_T, hist_alpha, matprop, pffmodel,
         weight_decay, num_epochs, optimizer, hist_Y_max_over_H=None, hist_alpha_bar=None,
         intermediateModel_path=None, writer=None, training_dict={}):
     loss_data = list()
-    
+    step_counter = 0
+
     # Loop over epochs
     for epoch in range(num_epochs):
         loop = tqdm(training_set_collocation, miniters=25)
         # Loop over batches
         for j, (inp_train, outp_train) in enumerate(loop):
-            
+
+            loss_cache = {}
+
             def closure():
                 optimizer.zero_grad()
                 if T_conn is None:
@@ -66,31 +69,41 @@ def fit(field_comp, training_set_collocation, T_conn, area_T, hist_alpha, matpro
 
                 loss = loss_var + weight_decay * loss_reg
 
-                if writer is not None:
-                    writer.add_scalars(
-                        'U_p_' + str(field_comp.lmbda.item()),
-                        {'loss': loss.item(), "loss_E": loss_var.item()},
-                        epoch
-                    )
-
-                loop.set_description(f"U_p: {field_comp.lmbda}, Epoch [{epoch}/{num_epochs}]")
-                loop.set_postfix(loss=loss.item(), loss_E=loss_var.item())
-                
-                loss_data.append(loss.item())
-                if intermediateModel_path is not None:
-                    idx = len(loss_data)
-                    steps = training_dict["save_model_every_n"]
-                    if steps > 0 and idx >= steps and idx % steps == 0:
-                        intermModel_path = intermediateModel_path / Path(
-                            'intermediate_1NN_' + str(int(field_comp.lmbda * 1000000)) +
-                            'by1000000_' + str(idx) + '.pt'
-                        )
-                        torch.save(field_comp.net.state_dict(), intermModel_path)
+                loss_cache["loss"] = loss.detach()
+                loss_cache["loss_E"] = loss_var.detach()
 
                 loss.backward()
                 return loss
-            
-            optimizer.step(closure=closure)
+
+            loss = optimizer.step(closure=closure)
+            step_counter += 1
+
+            # Retrieve cached loss values from the last closure call
+            loss_value = loss_cache.get("loss")
+            loss_E_value = loss_cache.get("loss_E", None)
+            if loss_value is None:
+                loss_value = loss.detach() if torch.is_tensor(loss) else torch.tensor(loss)
+
+            if writer is not None and loss_E_value is not None:
+                writer.add_scalars(
+                    'U_p_' + str(field_comp.lmbda.item()),
+                    {'loss': loss_value.item(), "loss_E": loss_E_value.item()},
+                    epoch
+                )
+
+            loop.set_description(f"U_p: {field_comp.lmbda}, Epoch [{epoch}/{num_epochs}]")
+            loop.set_postfix(loss=loss_value.item(),
+                             loss_E=loss_E_value.item() if loss_E_value is not None else None)
+
+            loss_data.append(loss_value.item())
+            if intermediateModel_path is not None:
+                steps = training_dict["save_model_every_n"]
+                if steps > 0 and step_counter >= steps and step_counter % steps == 0:
+                    intermModel_path = intermediateModel_path / Path(
+                        'intermediate_1NN_' + str(int(field_comp.lmbda * 1000000)) +
+                        'by1000000_' + str(step_counter) + '.pt'
+                    )
+                    torch.save(field_comp.net.state_dict(), intermModel_path)
 
     return loss_data
 
@@ -101,8 +114,10 @@ def fit_with_early_stopping(field_comp, training_set_collocation, T_conn, area_T
                             hist_Y_max_over_H=None, hist_alpha_bar=None, intermediateModel_path=None,
                             writer=None, training_dict={}):
     loss_data = list()
+    step_counter = 0
     early_stopping = EarlyStopping(tol_steps=10, min_delta=min_delta, device=area_T.device)
     loss_prev = torch.tensor([0.0], device=area_T.device)
+    epoch_loss = loss_prev
     
     # Loop over epochs
     for epoch in range(num_epochs):
@@ -134,33 +149,38 @@ def fit_with_early_stopping(field_comp, training_set_collocation, T_conn, area_T
 
             loss = loss_var + weight_decay * loss_reg
 
+            loss.backward()
+            optimizer.step()
+            step_counter += 1
+
+            loss_value = loss.detach()
+            loss_E_value = loss_var.detach()
+
             if writer is not None:
                 writer.add_scalars(
                     'U_p_' + str(field_comp.lmbda.item()),
-                    {'loss': loss.item(), "loss_E": loss_var.item()},
+                    {'loss': loss_value.item(), "loss_E": loss_E_value.item()},
                     epoch
                 )
 
             loop.set_description(f"U_p: {field_comp.lmbda}, Epoch [{epoch}/{num_epochs}]")
-            loop.set_postfix(loss=loss.item(), loss_E=loss_var.item())
+            loop.set_postfix(loss=loss_value.item(), loss_E=loss_E_value.item())
 
-            loss_data.append(loss.item())
+            loss_data.append(loss_value.item())
             if intermediateModel_path is not None:
-                idx = len(loss_data)
                 steps = training_dict["save_model_every_n"]
-                if steps > 0 and idx >= steps and idx % steps == 0:
+                if steps > 0 and step_counter >= steps and step_counter % steps == 0:
                     intermModel_path = intermediateModel_path / Path(
                         'intermediate_1NN_' + str(int(field_comp.lmbda * 1000000)) +
-                        'by1000000_' + str(idx) + '.pt'
+                        'by1000000_' + str(step_counter) + '.pt'
                     )
                     torch.save(field_comp.net.state_dict(), intermModel_path)
 
-            loss.backward()
-            optimizer.step()
-            
-        early_stopping(loss, loss_prev)
+            epoch_loss = loss_value
+
+        early_stopping(epoch_loss, loss_prev)
         if early_stopping.early_stop:
             break
-        loss_prev = loss
+        loss_prev = epoch_loss
 
     return loss_data
